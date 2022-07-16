53ede69c8771aec428c92715afff3de3f58f66b1
Felix <letopis666@gmail.com>
2022-07-16 21:01:58 +0600

    new change

---
+++ DZ3/main.js
@@ -0,0 +1,23 @@
+ function twoNumber(num1,num2){
+    if (num1 > num2){
+        console.log("num1 > num2")
+    } else if (num1 < num2){
+        console.log("num1 < num2")
+    }else {
+        console.log("===")
+    }
+ }
+ const num1 = 6
+ const num2 = 10
+twoNumber(num1, num2)
+
+
+// let digit = "Felix"
+
+const num1 = "Felix"
+const num2 = "Ramankulov"
+function CountChar (num1,num2){
+    console.count(num1.length)
+    console.count(num2.length)
+}
+CountChar(num1, num2)
